087a18963f5dc33813f0174569e6ed24cd6fb790
pandeYttn <60639420+pandeYttn@users.noreply.github.com>
2020-02-18 15:36:54 +0530

    Update ReverseDeleteString.java

--- ReverseDeleteString.java
+++ ReverseDeleteString.java
@@ -1,11 +1,13 @@
 public class ReverseDeleteString {
     public static void main(String[] args) {
          String s="vinodprajjwal";
+        System.out.println("original string:"+s);
         StringBuffer sb=new StringBuffer(s);
         //here we convert buffer into string
 
 
         //here we reverse the  String
+        System.out.println("after reverse and delete index from 4 to 9");
         System.out.println(sb.reverse().delete(4,9));
 
 
