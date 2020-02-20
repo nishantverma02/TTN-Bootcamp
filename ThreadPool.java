e57a13bbeec1617f9e393ddc4d39b366dcb82a7e
Prajjwal <prajjwal.pandey@tothenew.com>
2020-02-20 21:56:52 +0530

    java3

---
+++ ThreadPool.java
@@ -0,0 +1,36 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+class Even implements Runnable{
+
+    @Override
+    public void run() {
+        for(int i=2;i<=20;i=i+2 )
+        System.out.println(i);
+    }
+}
+
+class Odd implements Runnable{
+    @Override
+    public void run() {
+        for(int i=1;i<=20;i=i+2)
+            System.out.println(i);
+    }
+}
+
+public class ThreadPool {
+
+    public static void main(String[] args) {
+
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        executor.submit(new Odd());
+        executor.submit(new Even());
+
+        executor.shutdown();
+
+
+
+    }
+
+
+}
